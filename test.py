edd6ea8791235a617c266c3f52157c0dcdabf91b
shrrynsh <shrynshmodi@gmail.com>
2026-03-22 13:37:40 +0000

    feat: add initial implementation of DeepFool test script

---
+++ test.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import totrchvision
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt 
+import torch 
+import torch.optim as optim
+import torch.utils.data as data_utils
+from torch.autograd import Variable
+import math
+import torchvision.models as models
+from PIL import Image
+from deepfool import deepfool
+import os
+
+net=models.resnet34(pretrained=True)
+
+net.eval()
+
+im_orig=Image.open('img_path')
+mean=[0.485,0.456,0.406]
+std=[0.229,0.224,0.225]
+
+im=transofrms.Compose([
+    transforms.Scale(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean,std=std)
+])(im_orig)
+
+
+r,loop_i,label_orig,label_pert,pert_image=deepfool(im,net)
+labels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
+
+
